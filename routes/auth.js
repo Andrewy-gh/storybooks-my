6482ea933b30e50d0f4e6fb603f2b50d7ca220b1
Andy-git985 <andy.yu617@gmail.com>
2022-08-14 00:57:07 -0400

    logout async

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,13 @@ router.get(
 
 // @desc Logout use
 // @route /auth/logout
-router.get('logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
