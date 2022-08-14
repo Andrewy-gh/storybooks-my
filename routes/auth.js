a7d27f5e335726f50bb0d52561d60961de58ae5b
Andy-git985 <andy.yu617@gmail.com>
2022-08-13 22:46:40 -0400

    passport new user

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 // @desc Auth with Google
-// @route GET /
-router.get('/', (req, res) => {
-  res.render('login', { layout: 'login' });
-});
-// @desc Dashboard
-// @route GET /dasboard
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
-});
+// @route GET /auth/google
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+// @desc Google auth callback
+// @route GET /auth/google/callback
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
+);
 
 module.exports = router;
